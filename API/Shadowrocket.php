d09491006881d859c12bdd0f816c074362d122bb
BurpSuite <BurpSuite@iCloud.com>
2016-07-24 08:02:00 +0800

    3.2
    3.2

--- API/Shadowrocket.php
+++ API/Shadowrocket.php
@@ -4,9 +4,6 @@ header("cache-control:no-cache,must-revalidate");//No-Cache
 header("Content-Type:text/html;charset=UTF-8");//UTF-8
 //-------------通用-------------//
 $NAME = "UPlus";            //名称
-$ProxyRU = ",Proxy";        //其他
-$DIRECTRU = ",DIRECT";      //其他
-$REJECTRU = ",REJECT";      //其他
 //-------------文件-------------//
 $DefaultFile = "http://7xpphx.com1.z0.glb.clouddn.com/Proxy/File/Default.txt";
 $DefaultFile  = $DefaultFile . '?Cache='.time();
@@ -14,9 +11,6 @@ $Default = fopen($DefaultFile,"r");
 $ProxyFile = "http://7xpphx.com1.z0.glb.clouddn.com/Proxy/File/Proxy.txt";
 $ProxyFile  = $ProxyFile . '?Cache='.time();
 $Proxy = fopen($ProxyFile,"r");
-$GFWListFile = "http://7xpphx.com1.z0.glb.clouddn.com/Proxy/File/GFWList.txt";
-$GFWListFile  = $GFWListFile . '?Cache='.time();
-$GFWList = fopen($GFWListFile,"r");
 $DIRECTFile = "http://7xpphx.com1.z0.glb.clouddn.com/Proxy/File/DIRECT.txt";
 $DIRECTFile  = $DIRECTFile . '?Cache='.time();
 $DIRECT = fopen($DIRECTFile,"r");
@@ -54,7 +48,7 @@ echo "[Rule]";
 echo"\r\n# Default\r\n";
 while(!feof($Default))
 {
-echo trim(fgets($Default)).$DIRECTRU."\r\n"; 
+echo trim(fgets($Default)).",DIRECT"."\r\n"; 
 }
 {
 fclose($Default);
@@ -67,23 +61,10 @@ if($Proxy){//判断打开错误
 echo"# PROXY\r\n";
 while(!feof($Proxy))
 {
-echo trim(fgets($Proxy)).$ProxyRU."\r\n"; 
+echo trim(fgets($Proxy)).",Proxy"."\r\n"; 
 }
 {
 fclose($Proxy);
-}
-}else {
-  echo "下载失败!";//
-}
-//GFWList
-if($GFWList){//判断打开错误
-echo"# GFWList\r\n";
-while(!feof($GFWList))
-{
-echo trim(fgets($GFWList)).$ProxyRU."\r\n"; 
-}
-{
-fclose($GFWList);
 }
 }else {
   echo "下载失败!";//
@@ -93,7 +74,7 @@ if($DIRECT){//判断打开错误
 echo"# DIRECT\r\n";
 while(!feof($DIRECT))
 {
-echo trim(fgets($DIRECT)).$DIRECTRU."\r\n"; 
+echo trim(fgets($DIRECT)).",DIRECT"."\r\n"; 
 }
 {
 fclose($DIRECT);
@@ -106,7 +87,7 @@ if($REJECT){//判断打开错误
 echo"# REJECT\r\n";
 while(!feof($REJECT))
 {
-echo trim(fgets($REJECT)).$REJECTRU."\r\n"; 
+echo trim(fgets($REJECT)).",REJECT"."\r\n"; 
 }
 {
 fclose($REJECT);
